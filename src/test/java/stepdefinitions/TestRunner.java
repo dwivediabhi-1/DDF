39e838ad7a9e21b2262270ea112cc49df89c3d2b
Abhishek Dwivedi <abhishek.dwivedi@crestechsoftware.com>
2023-04-11 09:55:27 +0530

    Commented Tag To Run All Test Cases

--- src/test/java/stepdefinitions/TestRunner.java
+++ src/test/java/stepdefinitions/TestRunner.java
@@ -12,7 +12,7 @@ import io.cucumber.junit.CucumberOptions;
 @CucumberOptions(
 			features = "src//test//java//features",
 			glue = "stepdefinitions",
-			tags = "@tag3",
+			//tags = "@tag3",
 			plugin = { "pretty",  "html:test-output", "html:target/cucumber-report/cucumber.html",
 	                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:", "timeline:test-output-thread/",
 	                "json:target/cucumber-reports/Cucumber.json" }
